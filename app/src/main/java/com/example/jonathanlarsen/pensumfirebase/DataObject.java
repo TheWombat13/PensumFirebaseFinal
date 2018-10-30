98a0c9e27c61cec0216984132a35c0b21ba93f5e
JakobsenSWT <Jakobsen1608@hotmail.com>
2018-10-30 16:57:49 +0100

    Test Data implemented

--- app/src/main/java/com/example/jonathanlarsen/pensumfirebase/DataObject.java
+++ app/src/main/java/com/example/jonathanlarsen/pensumfirebase/DataObject.java
@@ -4,45 +4,39 @@ import com.example.jonathanlarsen.pensumfirebase.Litterature.LitteratureModel;
 import com.example.jonathanlarsen.pensumfirebase.PensumList.PensumModel;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class DataObject implements Serializable {
 
-    public static List<String> overview;
-
     /*
-    Hashmap <overview, pensumList>
+    PensumList is shown on the frontpage as headers for each cell
+    Cell data is gathered from pensumData.
      */
-    public static HashMap<String, List<String>> pensumListView;
+    public static List<String> pensumList;
     public static HashMap<String, PensumModel> pensumData;
 
     /*
-    List with keys for each sub-element of the frontpage-overview
-     */
-    public static HashMap<String, List<String>> pensumList;
-
-
-    /*
-    Hashmap <pensumList, litterature>
+    litteratureListView Hashmap <pensumList, litterature>
      */
     public static HashMap<String, List<String>> litteratureListView;
-    public static HashMap<String, LitteratureModel> litteratureData;
 
     /*
-    List with keys for each sub-sub-element of the frontpage-overview OR
+    List with keys for each sub-element of the frontpage-pensumList OR
     the sub-elements of each pensum
      */
     public static HashMap<String, List<String>> litterature;
+    public static HashMap<String, LitteratureModel> litteratureData;
 
     public DataObject () {
-        pensumListView = new HashMap<>();
-        pensumData = new HashMap<>();
-        pensumList = new HashMap<>();
+        pensumList = new ArrayList<>();
 
+        pensumData = new HashMap<>();
+
+        litterature = new HashMap<>();
         litteratureListView = new HashMap<>();
         litteratureData = new HashMap<>();
-        litterature = new HashMap<>();
     }
 
 }
--- app/src/main/java/com/example/jonathanlarsen/pensumfirebase/DataObject_Test.java
+++ app/src/main/java/com/example/jonathanlarsen/pensumfirebase/DataObject_Test.java
@@ -1,9 +1,13 @@
 package com.example.jonathanlarsen.pensumfirebase;
 
+import android.util.Log;
+
 import com.example.jonathanlarsen.pensumfirebase.Litterature.LitteratureModel;
 import com.example.jonathanlarsen.pensumfirebase.PensumList.PensumModel;
 
 import java.util.Objects;
+
+import static com.example.jonathanlarsen.pensumfirebase.MainActivity.TAG;
 
 public class DataObject_Test extends DataObject {
 
@@ -14,161 +18,120 @@ public class DataObject_Test extends DataObject {
     }
 
     private void DataObject_Initialer () {
-        overview.add(0, "Kunst-Historie");
-        overview.add(1, "Litteraturhistorie 2");
-        overview.add(2, "Dansk");
-
-        pensumListView.put(overview.get(0), pensumList.get(overview.get(0)));
-        pensumListView.put(overview.get(1), pensumList.get(overview.get(1)));
-        pensumListView.put(overview.get(2), pensumList.get(overview.get(2)));
-
-        pensumData.put(overview.get(0), new PensumModel());
-        pensumData.put(overview.get(1), new PensumModel());
-        pensumData.put(overview.get(2), new PensumModel());
+        /*
+        Initializing the List to be loaded as frontpage listview with each needed pensum
+         */
+        pensumList.add(0, "Kunst-Historie");
+        pensumList.add(1, "Litteraturhistorie 2");
+        pensumList.add(2, "Dansk");
 
         /*
+        Setting headers for each pensum created, here we set the headers as:
+            Kunst-historie
+            Litteraturhistorie 2
+            Dansk
+         */
+        litteratureListView.put(pensumList.get(0), litterature.get(pensumList.get(0)));
+        litteratureListView.put(pensumList.get(1), litterature.get(pensumList.get(1)));
+        litteratureListView.put(pensumList.get(2), litterature.get(pensumList.get(2)));
+
+        /*
+        Applying meta-data for each pensum
         Data model for respectively:
             Kunst-historie
             Litteraturhistorie 2
             Dansk
          */
-        Objects.requireNonNull(pensumData.get(overview.get(0))).setTitle(overview.get(0));
-        Objects.requireNonNull(pensumData.get(overview.get(0))).setPages(0);
-        Objects.requireNonNull(pensumData.get(overview.get(0))).setPagesToGo(1200);
-        Objects.requireNonNull(pensumData.get(overview.get(0))).setTeacher("Anders Andersen");
+        pensumData.put(pensumList.get(0), new PensumModel());
+        Objects.requireNonNull(pensumData.get(pensumList.get(0))).setTitle(pensumList.get(0));
+        Objects.requireNonNull(pensumData.get(pensumList.get(0))).setPages(0);
+        Objects.requireNonNull(pensumData.get(pensumList.get(0))).setPagesToGo(1200);
+        Objects.requireNonNull(pensumData.get(pensumList.get(0))).setTeacher("Anders Andersen");
 
-        Objects.requireNonNull(pensumData.get(overview.get(1))).setTitle(overview.get(1));
-        Objects.requireNonNull(pensumData.get(overview.get(1))).setPages(0);
-        Objects.requireNonNull(pensumData.get(overview.get(1))).setPagesToGo(500);
-        Objects.requireNonNull(pensumData.get(overview.get(1))).setTeacher("Kim Byvald");
+        pensumData.put(pensumList.get(1), new PensumModel());
+        Objects.requireNonNull(pensumData.get(pensumList.get(1))).setTitle(pensumList.get(1));
+        Objects.requireNonNull(pensumData.get(pensumList.get(1))).setPages(0);
+        Objects.requireNonNull(pensumData.get(pensumList.get(1))).setPagesToGo(500);
+        Objects.requireNonNull(pensumData.get(pensumList.get(1))).setTeacher("Kim Byvald");
 
-        Objects.requireNonNull(pensumData.get(overview.get(2))).setTitle(overview.get(2));
-        Objects.requireNonNull(pensumData.get(overview.get(2))).setPages(0);
-        Objects.requireNonNull(pensumData.get(overview.get(2))).setPagesToGo(900);
-        Objects.requireNonNull(pensumData.get(overview.get(2))).setTeacher("JØK");
+        pensumData.put(pensumList.get(2), new PensumModel());
+        Objects.requireNonNull(pensumData.get(pensumList.get(2))).setTitle(pensumList.get(2));
+        Objects.requireNonNull(pensumData.get(pensumList.get(2))).setPages(0);
+        Objects.requireNonNull(pensumData.get(pensumList.get(2))).setPagesToGo(900);
+        Objects.requireNonNull(pensumData.get(pensumList.get(2))).setTeacher("JØK");
+
+                /*
+        Adding the litterature list to its parent
+        For respectively:
+            Kunst-historie
+            Litteraturhistorie 2
+            Dansk
+         */
+        litteratureListView.put(pensumData.get(pensumList.get(0)).getTitle(),
+                litterature.get(pensumData.get(pensumList.get(0)).getTitle()));
+
+        litteratureListView.put(pensumData.get(pensumList.get(1)).getTitle(),
+                litterature.get(pensumData.get(pensumList.get(1)).getTitle()));
+
+        litteratureListView.put(pensumData.get(pensumList.get(2)).getTitle(),
+                litterature.get(pensumData.get(pensumList.get(2)).getTitle()));
 
         /*
         Adding the litterature-data & meta-data for each sub litterature for respectively:
             Kunst-historie
             Litteraturhistorie 2
             Dansk
+
+            How to get litteratureData:
+            pensumListView.get(pensumList.get( - Your pensumList - )).get( - Your Chosen listview - )
          */
         litteratureModel = new LitteratureModel("Test1", "", "", "", "", 1, 1, 2 );
-        litteratureData.put(pensumListView.get(overview.get(0)).get(0), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test2", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(0)).get(1), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test3", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(0)).get(2), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test4", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(0)).get(3), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
 
         litteratureModel = new LitteratureModel("Test5", "", "", "", "", 1, 1, 2 );
-        litteratureData.put(pensumListView.get(overview.get(1)).get(0), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test6", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(1)).get(1), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test7", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(1)).get(2), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test8", "", "", "", "", 1, 1, 2 );
-        litteratureData.put(pensumListView.get(overview.get(1)).get(3), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test9", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(1)).get(4), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test10", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(1)).get(5), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test11", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(1)).get(6), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test12", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(1)).get(7), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
 
         litteratureModel = new LitteratureModel("Test13", "", "", "", "", 1, 1, 2 );
-        litteratureData.put(pensumListView.get(overview.get(2)).get(0), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test14", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(2)).get(1), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
         litteratureModel = new LitteratureModel("Test15", "", "", "", "", 1, 2, 3);
-        litteratureData.put(pensumListView.get(overview.get(2)).get(2), litteratureModel);
+        litteratureData.put(litteratureModel.getTitle(), litteratureModel);
 
-        /*
-        Adding the litterature to its factions list for respectively
-            Kunst-historie
-            Litteraturhistorie 2
-            Dansk
-         */
-        litterature.get(pensumData.get(overview.get(0)).getTitle()).add(0,
-                litteratureData.get(pensumListView.get(overview.get(0)).get(0)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(0)).getTitle()).add(1,
-                litteratureData.get(pensumListView.get(overview.get(0)).get(1)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(0)).getTitle()).add(2,
-                litteratureData.get(pensumListView.get(overview.get(0)).get(2)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(0)).getTitle()).add(3,
-                litteratureData.get(pensumListView.get(overview.get(0)).get(3)).getTitle());
-
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(0,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(0)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(1,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(1)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(2,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(2)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(3,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(3)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(4,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(4)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(5,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(5)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(6,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(6)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(1)).getTitle()).add(7,
-                litteratureData.get(pensumListView.get(overview.get(1)).get(7)).getTitle());
-
-
-        litterature.get(pensumData.get(overview.get(2)).getTitle()).add(0,
-                litteratureData.get(pensumListView.get(overview.get(2)).get(0)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(2)).getTitle()).add(1,
-                litteratureData.get(pensumListView.get(overview.get(2)).get(1)).getTitle());
-
-        litterature.get(pensumData.get(overview.get(2)).getTitle()).add(2,
-                litteratureData.get(pensumListView.get(overview.get(2)).get(2)).getTitle());
-
-
-
-        /*
-        Adding the litterature list to its root
-        For respectively:
-            Kunst-historie
-            Litteraturhistorie 2
-            Dansk
-         */
-        litteratureListView.put(pensumData.get(overview.get(0)).getTitle(),
-                litterature.get(pensumData.get(overview.get(0)).getTitle()));
-
-        litteratureListView.put(pensumData.get(overview.get(1)).getTitle(),
-                litterature.get(pensumData.get(overview.get(1)).getTitle()));
-
-        litteratureListView.put(pensumData.get(overview.get(2)).getTitle(),
-                litterature.get(pensumData.get(overview.get(2)).getTitle()));
+        Log.d(TAG, "So far so fucking good!");
     }
 }
