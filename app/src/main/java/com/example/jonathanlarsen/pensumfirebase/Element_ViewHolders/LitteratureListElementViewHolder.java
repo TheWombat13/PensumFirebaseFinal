739fefa8e2e6d3a3a8fad2be2fc82b0c29c895d1
JakobsenSWT <Jakobsen1608@hotmail.com>
2018-11-03 15:32:05 +0100

    Code clean up, DataObject changes, class seperation etc.

--- app/src/main/java/com/example/jonathanlarsen/pensumfirebase/Element_ViewHolders/LitteratureListElementViewHolder.java
+++ app/src/main/java/com/example/jonathanlarsen/pensumfirebase/Element_ViewHolders/LitteratureListElementViewHolder.java
@@ -3,11 +3,10 @@ package com.example.jonathanlarsen.pensumfirebase.Element_ViewHolders;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LitteratureListElementViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+public class LitteratureListElementViewHolder extends RecyclerView.ViewHolder {
 
     public TextView title;
     public TextView author;
@@ -20,31 +19,7 @@ public class LitteratureListElementViewHolder extends RecyclerView.ViewHolder im
 
     public CheckBox delete;
 
-
-
     public LitteratureListElementViewHolder(View itemView) {
         super(itemView);
-        itemView.setOnClickListener(this);
-        itemView.setOnLongClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        }
-
-    /*
-    * onLongClick for enabling delete and possibly editing.
-    */
-    @Override
-    public boolean onLongClick(View v) {
-
-        //ToDo er kun synlig på en celle ad gangen! Visibility på checkbox er sat i xml pt.
-        delete.setVisibility(View.VISIBLE);
-
-        Toast.makeText(v.getContext(), "Hej", Toast.LENGTH_SHORT).show();
-
-
-        return true;
     }
 }
--- app/src/main/java/com/example/jonathanlarsen/pensumfirebase/Litterature/Litterature_Fragment.java
+++ app/src/main/java/com/example/jonathanlarsen/pensumfirebase/Litterature/Litterature_Fragment.java
@@ -1,11 +1,17 @@
 package com.example.jonathanlarsen.pensumfirebase.Litterature;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.jonathanlarsen.pensumfirebase.Adapter.Litterature_Adapter;
+import com.example.jonathanlarsen.pensumfirebase.Element_ViewHolders.LitteratureListElementViewHolder;
 import com.example.jonathanlarsen.pensumfirebase.R;
 
 import androidx.core.content.ContextCompat;
@@ -15,6 +21,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import static com.example.jonathanlarsen.pensumfirebase.MainActivity.PENSUM_BUNDLE_KEY;
+import static com.example.jonathanlarsen.pensumfirebase.MainActivity.TAG;
 
 public class Litterature_Fragment extends Fragment {
 
@@ -34,6 +41,28 @@ public class Litterature_Fragment extends Fragment {
         adapter = new Litterature_Adapter();
         recyclerView.setAdapter(adapter);
 
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getContext(),
+                recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //ToDO add onClick for litterature elements.
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+                Log.d(TAG, "OnLongClick: " + view.getId());
+
+                //ToDO doesn't work yet
+                for (int i = 0; i < recyclerView.getChildCount(); i++) {
+                    LitteratureListElementViewHolder viewHolder =
+                            (LitteratureListElementViewHolder) recyclerView.findViewHolderForAdapterPosition(i);
+                    viewHolder.delete.setVisibility(View.GONE);
+                }
+                Toast.makeText(view.getContext(), "Hej", Toast.LENGTH_SHORT).show();
+            }
+        }));
+
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             pensumView = bundle.getInt(PENSUM_BUNDLE_KEY, 0);
@@ -47,7 +76,5 @@ public class Litterature_Fragment extends Fragment {
 
         return view;
     }
-
-
 
 }
