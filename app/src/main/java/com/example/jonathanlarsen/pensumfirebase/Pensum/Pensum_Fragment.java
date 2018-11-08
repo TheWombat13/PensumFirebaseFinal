9503c7288c8449d47f18e302f62bb6999614f38d
jonathanlarsen <jal@atol.dk>
2018-11-08 15:20:18 +0100

    Floating action button anim ect.

--- app/src/main/java/com/example/jonathanlarsen/pensumfirebase/Pensum/Pensum_Fragment.java
+++ app/src/main/java/com/example/jonathanlarsen/pensumfirebase/Pensum/Pensum_Fragment.java
@@ -5,39 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.jonathanlarsen.pensumfirebase.Adapter.PensumList_Adapter;
-import com.example.jonathanlarsen.pensumfirebase.Litterature.Litterature_Fragment;
+import com.example.jonathanlarsen.pensumfirebase.ExpandebleMenu;
 import com.example.jonathanlarsen.pensumfirebase.R;
-import com.example.jonathanlarsen.pensumfirebase.Storage_DataModels.InternalStorage;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.io.IOException;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import static com.example.jonathanlarsen.pensumfirebase.MainActivity.LITTERATUREDATA_OBJECT_KEY;
-import static com.example.jonathanlarsen.pensumfirebase.MainActivity.LITTERATURE_LIST_OBJECT_KEY;
-import static com.example.jonathanlarsen.pensumfirebase.MainActivity.PENSUMDATA_OBJECT_KEY;
-import static com.example.jonathanlarsen.pensumfirebase.MainActivity.PENSUM_LIST_OBJECT_KEY;
-import static com.example.jonathanlarsen.pensumfirebase.Storage_DataModels.DataObject.litteratureData;
-import static com.example.jonathanlarsen.pensumfirebase.Storage_DataModels.DataObject.litteratureListView;
-import static com.example.jonathanlarsen.pensumfirebase.Storage_DataModels.DataObject.pensumData;
-import static com.example.jonathanlarsen.pensumfirebase.Storage_DataModels.DataObject.pensumList;
 
-
-public class Pensum_Fragment extends Fragment {
+public class Pensum_Fragment extends Fragment implements View.OnClickListener{
 
     public static Context context;
     public static RecyclerView recyclerView;
     private PensumList_Adapter adapter;
-    private FloatingActionButton addPensumBtn;
+    private ExpandebleMenu expandebleMenu;
+
+    private Toast toast = null;
+
+    private Button addPensum, editPensum, sendPensum;
+
+    /*
+    private ArcLayout arcLayout;
+    private View expandMenu;
+    private View menuLayout;
+    */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,37 +45,163 @@ public class Pensum_Fragment extends Fragment {
 
         context = getContext();
 
+        expandebleMenu = new ExpandebleMenu();
+
         recyclerView = view.findViewById(R.id.listview_pensum);
-        addPensumBtn = view.findViewById(R.id.addPensumBtn);
+
+        addPensum = view.findViewById(R.id.add_pensum);
+        expandebleMenu.expandMenu = view.findViewById(R.id.expand_menu);
+        expandebleMenu.menuLayout = view.findViewById(R.id.menu_layout);
+        expandebleMenu.arcLayout = view.findViewById(R.id.arc_layout);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         adapter = new PensumList_Adapter();
+
         recyclerView.setAdapter(adapter);
+
+
 
         recyclerView.addItemDecoration(new DividerItemDecoration(this.getActivity(),
                 DividerItemDecoration.VERTICAL));
 
-        DividerItemDecoration itemDecorator = new DividerItemDecoration(this.getActivity(), DividerItemDecoration.VERTICAL);
-        itemDecorator.setDrawable(ContextCompat.getDrawable(this.getActivity(), R.drawable.pensum_divider));
+/*
+        for (int i = 0, size = arcLayout.getChildCount(); i < size; i++) {
+            arcLayout.getChildAt(i).setOnClickListener(this);
+        }
+*/
+        expandebleMenu.expandMenu.setOnClickListener(this);
+        addPensum.setOnClickListener(this);
 
-
-        addPensumBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                AddPensum_Fragment nextFrag = new AddPensum_Fragment();
-
-                AppCompatActivity activity = (AppCompatActivity) view.getContext();
-                activity.getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.MiddleContainer, nextFrag, "findThisFragment")
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
 
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.expand_menu) {
+            onFabClick(v);
+            return;
+        }
 
+        if (v.getId() == R.id.add_pensum) {
+            AddPensum_Fragment nextFrag = new AddPensum_Fragment();
+
+            AppCompatActivity activity = (AppCompatActivity) getContext();
+            activity.getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.MiddleContainer, nextFrag, "findThisFragment")
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+        if (v instanceof Button) {
+            showToast((Button) v);
+        }
+
+    }
+
+    private void showToast(Button btn) {
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        String text = "Clicked: " + btn.getText();
+        toast = Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    private void onFabClick(View v) {
+        if (v.isSelected()) {
+            expandebleMenu.hideMenu();
+        } else {
+            expandebleMenu.showMenu();
+        }
+        v.setSelected(!v.isSelected());
+    }
+
+/*
+    @SuppressWarnings("NewApi")
+    private void showMenu() {
+        menuLayout.setVisibility(View.VISIBLE);
+
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = 0, len = arcLayout.getChildCount(); i < len; i++) {
+            animList.add(createShowItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(400);
+        animSet.setInterpolator(new OvershootInterpolator());
+        animSet.playTogether(animList);
+        animSet.start();
+    }
+
+    @SuppressWarnings("NewApi")
+    private void hideMenu() {
+
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = arcLayout.getChildCount() - 1; i >= 0; i--) {
+            animList.add(createHideItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(400);
+        animSet.setInterpolator(new AnticipateInterpolator());
+        animSet.playTogether(animList);
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                menuLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+        animSet.start();
+
+    }
+
+    private Animator createShowItemAnimator(View item) {
+
+        float dx = expandMenu.getX() - item.getX();
+        float dy = expandMenu.getY() - item.getY();
+
+        item.setRotation(0f);
+        item.setTranslationX(dx);
+        item.setTranslationY(dy);
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.rotation(0f, 720f),
+                AnimatorUtils.translationX(dx, 0f),
+                AnimatorUtils.translationY(dy, 0f)
+        );
+
+        return anim;
+    }
+
+    private Animator createHideItemAnimator(final View item) {
+        float dx = expandMenu.getX() - item.getX();
+        float dy = expandMenu.getY() - item.getY();
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.rotation(720f, 0f),
+                AnimatorUtils.translationX(0f, dx),
+                AnimatorUtils.translationY(0f, dy)
+        );
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                item.setTranslationX(0f);
+                item.setTranslationY(0f);
+            }
+        });
+
+        return anim;
+    }
+*/
 }
 
